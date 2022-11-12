9957d3cd787cb7f458ff6f012aa83feded7b8e6c
Tim Slager <timtendo12@gmail.com>
2022-11-12 22:42:02 +0100

    refresh setup

--- src/Events/interactionCreate.js
+++
@@ -1,6 +0,0 @@
-
-const Event = require("../Structures/Event.js");
-
-module.exports = new Event("interactionCreate", (Client, Message,) => {
-
-});
--- src/index.js
+++
@@ -1,9 +0,0 @@
-console.clear(); //for nodemon
-
-const Client = require("./Structures/client.js");
-const config = require("./data/config.json");
-const Discord = require("discord.js");
-
-const client = new Client({ partials: ["CHANNEL"]});
-
-client.start(config.token);
